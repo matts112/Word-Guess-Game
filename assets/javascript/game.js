e35a65c0213adec16dfadf81b1bf1b23fc9fa58d
Matt S <mttswinford@yahoo.com>
2018-08-18 11:06:16 -0700

    my homework

---
+++ assets/javascript/game.js
@@ -0,0 +1,116 @@
+var selectableWords =           
+    [
+        "eva",
+        "seele",
+        "nerv",
+    ];
+const maxTries = 10; 
+var guessedLetters = [];
+var currentWordIndex;
+var guessingWord = [];
+var remainingGuesses = 0;
+var gameStarted = false;
+var hasFinished = false;
+var wins = 0;
+
+function resetGame() {
+    remainingGuesses = maxTries;
+    gameStarted = false;
+
+   
+    currentWordIndex = Math.floor(Math.random() * (selectableWords.length));
+
+    
+    guessedLetters = [];
+    guessingWord = [];
+
+    for (var i = 0; i < selectableWords[currentWordIndex].length; i++) {
+        guessingWord.push("_");
+    }
+    
+    document.getElementById("pressKeyTryAgain").style.cssText= "display: none";
+    document.getElementById("gameover-image").style.cssText = "display: none";
+    document.getElementById("youwin-image").style.cssText = "display: none";
+
+    
+    updateDisplay();
+};
+
+function updateDisplay() {
+
+    document.getElementById("totalWins").innerText = wins;
+    document.getElementById("currentWord").innerText = "";
+    for (var i = 0; i < guessingWord.length; i++) {
+        document.getElementById("currentWord").innerText += guessingWord[i];
+    }
+    document.getElementById("remainingGuesses").innerText = remainingGuesses;
+    document.getElementById("guessedLetters").innerText = guessedLetters;
+    if(remainingGuesses <= 0) {
+        document.getElementById("gameover-image").style.cssText = "display: block";
+        document.getElementById("pressKeyTryAgain").style.cssText = "display:block";
+        hasFinished = true;
+    }
+};
+
+document.onkeydown = function(event) {
+    
+    if(hasFinished) {
+        resetGame();
+        hasFinished = false;
+    } else {
+       
+        if(event.keyCode >= 65 && event.keyCode <= 90) {
+            makeGuess(event.key.toLowerCase());
+        }
+    }
+};
+
+function makeGuess(letter) {
+    if (remainingGuesses > 0) {
+        if (!gameStarted) {
+            gameStarted = true;
+        }
+
+       
+        if (guessedLetters.indexOf(letter) === -1) {
+            guessedLetters.push(letter);
+            evaluateGuess(letter);
+        }
+    }
+    
+    updateDisplay();
+    checkWin();
+};
+
+
+function evaluateGuess(letter) {
+    
+    var positions = [];
+
+   
+    for (var i = 0; i < selectableWords[currentWordIndex].length; i++) {
+        if(selectableWords[currentWordIndex][i] === letter) {
+            positions.push(i);
+        }
+    }
+
+    
+    if (positions.length <= 0) {
+        remainingGuesses--;
+        
+    } else {
+        
+        for(var i = 0; i < positions.length; i++) {
+            guessingWord[positions[i]] = letter;
+        }
+    }
+};
+
+function checkWin() {
+    if(guessingWord.indexOf("_") === -1) {
+        document.getElementById("youwin-image").style.cssText = "display: block";
+        document.getElementById("pressKeyTryAgain").style.cssText= "display: block";
+        wins++;
+        hasFinished = true;
+    }
+};S
